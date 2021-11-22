1c12f92dd3ff3af16024cc0da3abce5bbc126805
Liam Sho <conan_creeper@outlook.com>
2021-11-22 16:12:05 +0800

    doc.添加 Public 方法的 XML 文档

--- src/Web/KaiheilaSharp.Web.Webhook/Webhook.cs
+++ src/Web/KaiheilaSharp.Web.Webhook/Webhook.cs
@@ -47,12 +47,25 @@ public class Webhook
 
     #region 构造函数
 
+    /// <summary>
+    /// 创建 Webhook 实例，使用默认的监听 IP(127.0.0.1) 和
+    /// 端口(5000)设置，API 终结点为 <paramref name="endpoint"/>
+    /// </summary>
+    /// <param name="endpoint">API 终结点</param>
     public Webhook(string endpoint)
     {
         _ipAddress = IPAddress.Loopback;
         _port = 5000;
         _endpoint = endpoint;
     }
+
+    /// <summary>
+    /// 创建 Webhook 实例，设置监听 IP(<paramref name="ipAddress"/>) 和
+    /// 端口(<paramref name="port"/>)设置，API 终结点为 <paramref name="endpoint"/>
+    /// </summary>
+    /// <param name="endpoint">API 终结点</param>
+    /// <param name="ipAddress">监听 IP 地址</param>
+    /// <param name="port">监听端口</param>
     public Webhook(string endpoint, IPAddress ipAddress, int port)
     {
         _ipAddress = ipAddress;
@@ -62,26 +75,50 @@ public class Webhook
 
     #endregion
 
+    /// <summary>
+    /// 使用 <see cref="WebApplicationBuilder"/> 配置 <see cref="WebApplication"/>
+    /// </summary>
+    /// <param name="webApplicationConfigurator"><see cref="WebApplicationBuilder"/> Action 委托</param>
+    /// <returns>当前 Webhook 实例</returns>
     public Webhook WithWebApplicationConfiguration(Action<WebApplicationBuilder> webApplicationConfigurator)
     {
         _webApplicationConfigurator = webApplicationConfigurator;
         return this;
     }
+    /// <summary>
+    /// 设置使用加密密钥，如果未设置，收到加密信息时会抛出异常
+    /// </summary>
+    /// <param name="key">加密密钥</param>
+    /// <returns>当前 Webhook 实例</returns>
     public Webhook WithEncryptKey(string key)
     {
         _key = key;
         return this;
     }
+    /// <summary>
+    /// 设置机器人验证 Token，如果未设置，将不会进行检查
+    /// </summary>
+    /// <param name="verifyToken">验证 Token</param>
+    /// <returns>当前 Webhook 实例</returns>
     public Webhook WithVerifyToken(string verifyToken)
     {
         _verifyToken = verifyToken;
         return this;
     }
+    /// <summary>
+    /// 订阅消息事件
+    /// </summary>
+    /// <param name="delegate">消息事件处理委托</param>
+    /// <returns></returns>
     public Webhook Subscribe(WebhookMessageReceivedDelegate @delegate)
     {
         WebhookMessageReceivedEvent += @delegate;
         return this;
     }
+    /// <summary>
+    /// 开启 Kestrel 服务器
+    /// </summary>
+    /// <returns></returns>
     public Task Run()
     {
         var builder = WebApplication.CreateBuilder();
